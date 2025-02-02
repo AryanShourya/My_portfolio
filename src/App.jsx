8317e67b2abd632b83291315f87264422b1a561a
Aryan Shourya <aryanshourya98@gmail.com>
2025-02-03 04:35:53 +0530

    updated dark mode
    improved the dark mode to full body. now the dark mode inabled works perfectly.

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,21 @@ function App() {
     const [mode,setmode]=useState("white");
     const [textCol,setTextCol] = useState("gray-800");
     const possible =["text-gray-800", "bg-white","text-neutral-300","bg-black","shadow-gray-300",
-      "shadow-white"
+      "shadow-white","bg-slate-700"
     ];
 
     const changeTheme =()=>{
        setmode(mode=="white"?"black":"white");
        setTextCol(textCol=="gray-800"?"neutral-300":"gray-800");
+       
     }
   return (
     <>
+      {document.body.style.backgroundColor = mode}
+      <div className={`size-full justify-center bg-${mode} text-${textCol}` }>
       <Navbar name="SHOURYA" mode={mode} textColor={textCol} changeTheme={changeTheme}/>
-      <div className={`size-full mt-18 justify-center bg-${mode} text-${textCol} h-full` }>
       <Home mode={mode} />
-      <Technology />
+      <Technology mode={mode}/>
       </div>
     </>
   )
