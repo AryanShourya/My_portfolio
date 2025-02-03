b5616196bd6d35c202a4257bc825c1f4e8e1007c
Aryan Shourya <aryanshourya98@gmail.com>
2025-02-04 03:48:48 +0530

    updated external links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,35 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import CloseIcon from '@mui/icons-material/Close';
 import PropTypes from "prop-types"
 import { Link } from "react-scroll";
-
+import Sidebar from './Sidebar';
+import { useState } from 'react';
 
 export default function Navbar(props) {
-
+    const [sidebarToggle,setSidebarToggle] = useState(false);
+    const sidebarClick=()=>{
+        sidebarToggle==false?setSidebarToggle(true):setSidebarToggle(false);
+    }
     
   return (
+    <>
+    <Sidebar sbClick={sidebarClick} mode={props.mode} sbToggle={sidebarToggle} textColor={props.textColor}/> 
     <nav className={`fixed  bg-${props.mode}/80 text-${props.textColor}
                      md:justify-items-center backdrop-blur-lg z-50
                      content-center min-h-15 w-screen top-1 sm:min-w-[500px] 
                      border-neutral-300 border-b-[0.3px]`}> 
+
+        {/* This the navbar at screen width less than md               */}
       <div className="flex md:hidden justify-between">
         <ul className=" p-2 flex w-screen">
             <li >
-                <a href="/" className="text-xl font-bold">{props.name}</a> 
+                <Link to="home" smooth="true" offset={-70}
+                 className="text-xl font-bold">{props.name}</Link> 
             </li>
         </ul>
-        <ul className="flex p-2 space-x-4">
+        <ul className="flex py-2 px-4 space-x-4">
             <li >
                 <button onClick={props.changeTheme} 
                     className={`${props.mode =='white'?"hidden":"block"}`}>
@@ -33,13 +43,19 @@ export default function Navbar(props) {
             </li>
 
             <li >
-                <button >
-                <span className="material-symbols-outlined dark:fill-neutral-200">
+                <button onClick={sidebarClick}className={`${sidebarToggle==true?"hidden":"block"}`}>
+                <span className="material-symbols-outlined">
                     menu</span>
+                </button>
+
+                <button onClick={sidebarClick} className={`${sidebarToggle==false?"hidden":"block"}`}>
+                <CloseIcon/>
                 </button>
             </li>
         </ul>
       </div>
+
+      {/* Main navbar at full screen width  */}
       <div className="lg:max-w-5xl hidden md:flex bg-green  items-center
                       justify-between sm:w-screen ">
         <ul className="p-2 md:flex space-x-4 font-medium">
@@ -76,12 +92,12 @@ export default function Navbar(props) {
                 </button>
             </li>
             <li>
-                <a href="">
+                <a href="https://github.com/AryanShourya" target="_blank" rel="noopener noreferrer">
                     <GitHubIcon />
                 </a>
             </li>
             <li>
-                <a href="">
+                <a href="https://www.linkedin.com/in/aryan-shourya-6934a3253" target="_blank" rel="noopener noreferrer">
                     <LinkedInIcon />
                 </a>
             </li>
@@ -89,13 +105,13 @@ export default function Navbar(props) {
         </ul>
       </div>
     </nav>
+    </>
   );
 }
 
 Navbar.propTypes = {
     mode : PropTypes.string,
     textColor : PropTypes.string,
-    firstelement :PropTypes.string,
     name: PropTypes.string,
     changeTheme:PropTypes.func
 }
