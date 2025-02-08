0a24937f27b9e18c7a8a620a70213d099bbd1f57
Aryan Shourya <aryanshourya98@gmail.com>
2025-02-08 23:14:05 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Contact from './components/Contact'
 import './App.css'
 import { useState } from 'react'
 import Projects from './components/Projects'
+import { Copyright } from '@mui/icons-material'
 
 
 function App() {
@@ -29,6 +30,11 @@ function App() {
       <Projects/>
       <Contact mode={mode} textColor={textCol}/>
       </div>
+      <footer className='justify-items-center min-h-40 w-screen place-content-center'>
+        <div className={`justify-items-center items-center text-${textCol}`}>
+          <Copyright/>
+          Coded by Aryan Shourya,2025.</div>
+      </footer>
     </>
   )
 }
