0e5c5acd6f0f3295d532c168a6520815822f5190
Aryan Shourya <aryanshourya98@gmail.com>
2025-02-20 03:37:39 +0530

    added profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,12 @@
 //import React from 'react'
-
+import { Link } from "react-router-dom"
 export default function Profile() {
   return (
-    <div>
+    <div className="mt-60">
+      This is the Profile page.
+      <button className="bg-amber-200 outline-[0.1px] outline-gray-400 ml-10">
+        <Link className='p-2' to="/">Home Page</Link>
+      </button>
       
     </div>
   )
